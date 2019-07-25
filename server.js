b19fd887bc4a5a0f3cc1d34eb9e3dda61f9b8c40
raxemmohamed <raxemmohamed@gmail.com>
2019-07-25 10:31:31 -0500

    json web token

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+var jwt = require("jsonwebtoken");
 
 var db = require("./models");
 
@@ -22,6 +23,62 @@ app.engine(
 app.set("view engine", "handlebars");
 
 // Routes
+app.get('/api/test', (req, res) => {
+  res.json({
+    message: "Welcome"
+  });
+});
+
+app.post('/api/posts', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if(err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: 'Post created',
+        authData
+      });
+    }
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  //mock user
+  const user = {
+    id: 1,
+    username: "Raxem",
+  }
+
+  jwt.sign({user}, 'secretkey', (err, token) => {
+    res.json({
+      token
+    });
+  });
+});
+
+//format of token
+//authorizatiion: bearer <access_token>
+
+//verify token
+function verifyToken(req, res, next) {
+// get auth header value
+const bearerHeader = req.headers['authorization'];
+//check if bearer is undefinded
+if(typeof bearerHeader !== 'undefined') {
+//split at the space
+const bearer = bearerHeader.split(' ');
+//get token from array
+const bearerToken = bearer[1];
+//set the token
+req.token = bearerToken;
+//next middleware
+next();
+} else {
+  //forbidden
+  res.sendStatus(403);
+}
+}
+
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
