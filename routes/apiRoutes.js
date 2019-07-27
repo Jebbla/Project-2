b3b4a8cc59b123c55d219032bfbc387f7ca827af
Adam Grise <adamwgrise@gmail.com>
2019-07-27 09:14:50 -0500

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,37 +1,41 @@
 var db = require("../models");
 
 module.exports = function (app) {
-  //game stuff
+  // Game data
   var game = {
-    blanksArr:[],
-    category:'',
+    blanksArr: [],
+    category: '',
+    wheelValues:[],
   };
 
-
-
-  // Get all examples
+  // Start Round request; still needs to include generating the wheel values and send to front end
   app.get("/api/startRound", function (req, res) {
     db.GameSolution.findAll({}).then(function (solutions) {
-      var roundSolution = solutions[Math.floor(Math.random()*solutions.length)];
+      var roundSolution = solutions[Math.floor(Math.random() * solutions.length)];
       var phrase = roundSolution.dataValues.solution.toUpperCase();
       var phraseArr = phrase.split("");
       game.category = roundSolution.dataValues.category;
       game.blanksArr = [];
-      var genBlanks = function() {
+      var genBlanks = function () {
         for (i = 0; i < phraseArr.length; i++) {
           game.blanksArr.push("_");
         };
         for (i = 0; i < phraseArr.length; i++) {
           switch (phraseArr[i]) {
-            case " ":game.blanksArr[i] = " ";              
+            case " ":
+              game.blanksArr[i] = " ";
               break;
-            case "-":game.blanksArr[i] = "-";              
+            case "-":
+              game.blanksArr[i] = "-";
               break;
-            case "?":game.blanksArr[i] = "?";              
+            case "?":
+              game.blanksArr[i] = "?";
               break;
-            case ",":game.blanksArr[i] = ",";              
-              break;          
-            default:"_";
+            case ",":
+              game.blanksArr[i] = ",";
+              break;
+            default:
+              "_";
               break;
           };
         };
@@ -41,10 +45,21 @@ module.exports = function (app) {
       console.log(phrase);
       console.log(game.category);
       console.log(game.blanksArr.join(""));
-      
-
-      res.json(game);
     });
+    // Generate the wheel values from the DB as well
+    res.json(game);
+  });
+
+  // Can this value be used to determine the index of the spin?
+  app.get("/api/spinWheel", function (req, res) {
+    spinValue = function () {
+      Math.floor(Math.random() * 24);
+    }
+    res.json(spinValue);
+  });
+
+  app.get("/api/processGuess", function (req, res) {
+    // integrate several functions here from the front-end JS: guessDupeNLog, guessMatch, guessRevealorLose, guessIsWin, and youWin; essentially the whole "runGame" function
   });
 
   // Create a new example
@@ -52,7 +67,7 @@ module.exports = function (app) {
     db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
-  }); 
+  });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function (req, res) {
